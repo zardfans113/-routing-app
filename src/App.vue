f3c234ec9abc8d9086460b86bf5db5f7e6f46247
zardfans113 <jasontw77661@gmail.com>
2024-04-26 00:48:40 +0800

    update the appearance of all data with cards.

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
 </script>
 
 <template>
-	<main>
+	<main class='container'>
 		<h1>Our Cars</h1>
-		<div>
-			<div v-for='car in cars' :key='car.id'>
+		<div class='cards'>
+			<div v-for='car in cars' :key='car.id' class='card'>
 				<h1>{{ car.make }}</h1>
 				<p>${{ car.price }}</p>
 			</div>
@@ -18,4 +18,19 @@
 </template>
 
 <style scoped>
+  .cards {
+    display: flex;
+    width: 1000px;
+    flex-wrap: wrap;
+    margin-top: 50px;
+    justify-content: center;
+  }
+  .card {
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.207);
+    padding: 15px;
+    width: 150px;
+    margin-right: 15px;
+    cursor: pointer;
+    margin-bottom: 20px;
+  }
 </style>
